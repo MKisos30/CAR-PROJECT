27a0cc9b00ed87fcb7cf1b83063669c8bdbf0b49
Miriam Kisos <87091443+MKisos30@users.noreply.github.com>
2022-08-26 10:24:07 +0300

    aa

--- Ecommerce/BACKEND/Routes/Reserve.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express');
-const route = Router();
-const { saveReserveDetails } = require('../Controllers/Reserve');
-
-route
-    .post('/save-reserve', saveReserveDetails)
-
-module.exports = route;
